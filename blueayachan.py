078ec478345adfb2c457c5ae8778f29c7c89df82
Alex Barney <alex.f.barney@gmail.com>
2022-09-21 22:38:30 -0400

    removes case sensitivity

--- blueayachan.py
+++ blueayachan.py
@@ -1,6 +1,6 @@
 '''
 Project: BlueAyaChan - Twitch IRC Bot
-Date Published: 09/20/2022
+Date Published: 09/21/2022
 Date Created: 06/16/2021
 File: blueayachan.py
 Author: Alex Barney (electra_rta)
@@ -964,15 +964,16 @@ class BlueAyaChan(commands.Bot):
         print(f'{self.nick} is ready to launch')
     #log chats
     async def event_message(self, message):
+    
         curtime = datetime.now()
         print(f'[' + str(curtime.strftime("%H:%M:%S")) + '] #' + str(message.channel) + " <" + message.author.name +">: " + message.content)
         if(str(message.channel) in dreamboum_only):
             if(message.content.lower() == '!dreamboumtweet' or message.content.lower() == 'dreamboumtweet'):
-                await self.handle_commands(message)
+                await self.handle_commands(message.content.lower())
             else:
                 return
         if(str(message.channel) not in dreamboum_only):    
-            await self.handle_commands(message)
+            await self.handle_commands(message.content.lower())
     '''################- C O M M A N D S -################'''
     '''
     @commands.command(name='test')
@@ -1844,6 +1845,7 @@ class BlueAyaChan(commands.Bot):
     @commands.command(name='iloveshadowhearts:fromthenewworld')
     async def iloveshadowhearts(self, ctx):
         await ctx.send(f'{ctx.author.name} says,  "I love Shadow Hearts: From the New World"')
+    
     # -------------------------------------------------------------------------------------------------------------#
     #########################################   JOIN/LEAVE COMMANDS   ##############################################
     # -------------------------------------------------------------------------------------------------------------#
